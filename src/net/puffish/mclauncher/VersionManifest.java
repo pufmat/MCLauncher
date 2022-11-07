48870dd3915259c1b4d2e8124caa056a610651e5
pufmat <pufferfish@pufferfish.pl>
2022-11-07 14:52:32 +0100

    Fix custom version downloading

--- src/net/puffish/mclauncher/VersionManifest.java
+++ src/net/puffish/mclauncher/VersionManifest.java
@@ -8,64 +8,74 @@ import org.json.JSONObject;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Function;
 
-public class VersionManifest{
+public class VersionManifest {
 	private final GameDirectory gd;
 	private final OperationSystem os;
 
 	private final Option<JSONObject> json;
 
-	public VersionManifest(GameDirectory gd, OperationSystem os, Option<JSONObject> json){
+	public VersionManifest(GameDirectory gd, OperationSystem os, Option<JSONObject> json) {
 		this.gd = gd;
 		this.os = os;
 		this.json = json;
 	}
 
-	private Path getJsonPath(String versionName){
-		return gd.versions().resolve(versionName).resolve(versionName + ".json");
+	private Path getJsonPath(String versionName) {
+		return gd.versions()
+				.resolve(versionName)
+				.resolve(versionName + ".json");
 	}
 
-	private Either<Exception, URL> findDownloadUrl(String versionName){
+	private Either<Exception, URL> findDownloadUrl(String versionName) {
 		return json.<Either<Exception, URL>>map(x -> {
-			try {
-				JSONArray versionsJson = x.getJSONArray("versions");
-				for (Object obj : versionsJson) {
-					if (obj instanceof JSONObject versionJson) {
-						if (versionJson.getString("id").equals(versionName)) {
-							return Either.right(new URL(versionJson.getString("url")));
+					try {
+						JSONArray versionsJson = x.getJSONArray("versions");
+						for (Object obj : versionsJson) {
+							if (obj instanceof JSONObject versionJson) {
+								if (versionJson.getString("id").equals(versionName)) {
+									return Either.right(new URL(versionJson.getString("url")));
+								}
+							}
 						}
+					} catch (Exception e) {
+						return Either.left(e);
 					}
-				}
-			}catch (Exception e){
-				return Either.left(e);
-			}
-			return Either.left(new IllegalStateException("Version does not exists!"));
-		}).getOrElse(() -> Either.left(new IllegalStateException("Missing version manifest!")));
+					return Either.left(new IllegalStateException("Version `" + versionName + "` does not exists!"));
+				}).getOrElse(() -> Either.left(new IllegalStateException("Missing version manifest!")));
 	}
 
-	private Either<Exception, Version> getVersionInfo(Path path){
+	private Either<Exception, Version> getVersionInfo(Path path, Function<String, Either<Exception, Version>> supplier) {
 		try {
-			return Version.tryMake(gd, os, new JSONObject(Files.readString(path)), this::getVersionInfo);
-		}catch (Exception e){
+			return Version.tryMake(gd, os, new JSONObject(Files.readString(path)), supplier);
+		} catch (Exception e) {
 			return Either.left(e);
 		}
 	}
 
-	public Either<Exception, Version> getVersionInfo(String versionName){
-		return getVersionInfo(getJsonPath(versionName));
+	private Either<Exception, Version> getVersionInfo(String versionName, Function<String, Either<Exception, Version>> supplier) {
+		return getVersionInfo(getJsonPath(versionName), supplier);
 	}
 
-	public Either<Exception, Version> downloadVersionInfo(String versionName, URL versionUrl, DownloadHandler dh){
+	public Either<Exception, Version> getVersionInfo(String versionName) {
+		return getVersionInfo(versionName, this::getVersionInfo);
+	}
+
+	public Either<Exception, Version> downloadVersionInfo(String versionName, URL versionUrl, DownloadHandler dh) {
 		try {
 			return dh.downloadToFileAndString(versionUrl, getJsonPath(versionName))
 					.flatMap(x -> Version.tryMake(gd, os, new JSONObject(x), y -> this.downloadVersionInfo(y, dh)));
-		}catch (Exception e){
+		} catch (Exception e) {
 			return Either.left(e);
 		}
 	}
 
-	public Either<Exception, Version> downloadVersionInfo(String versionName, DownloadHandler dh){
+	public Either<Exception, Version> downloadVersionInfo(String versionName, DownloadHandler dh) {
 		return findDownloadUrl(versionName)
-				.flatMap(versionUrl -> downloadVersionInfo(versionName, versionUrl, dh));
+				.fold(
+						e -> getVersionInfo(versionName, x -> downloadVersionInfo(x, dh)),
+						versionUrl -> downloadVersionInfo(versionName, versionUrl, dh)
+				);
 	}
 }
