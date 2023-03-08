c88937d79d0bc6f6a9f4c3e4ed2bca3592500938
pufmat <pufferfish@pufferfish.pl>
2023-03-08 15:20:32 +0100

    Fix variables replacing

--- src/net/puffish/mclauncher/VariablesReplacer.java
+++ src/net/puffish/mclauncher/VariablesReplacer.java
@@ -1,10 +1,11 @@
 package net.puffish.mclauncher;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class VariablesReplacer {
-	public record Variable(String name, String... replacements){}
+	public record Variable(String name, String... replacements) {}
 
 	private final List<Variable> variables;
 
@@ -12,21 +13,25 @@ public class VariablesReplacer {
 		this.variables = variables;
 	}
 
-	public List<String> replace(String str){
-		for(Variable variable : variables){
-			int index = str.indexOf(variable.name());
-			if(index != -1){
-				List<String> list = Arrays.asList(variable.replacements());
-				list.set(
-						0,
-						str.substring(0, index) + list.get(0)
-				);
-				list.set(
-						list.size() - 1,
-						list.get(list.size() - 1) + str.substring(index + variable.name().length())
-				);
-				return list;
+	public List<String> replace(String str) {
+		for (Variable variable : variables) {
+			var index = str.indexOf(variable.name());
+			if (index == -1) {
+				continue;
 			}
+
+			var list = new ArrayList<>(Arrays.asList(variable.replacements()));
+			list.set(
+					0,
+					str.substring(0, index) + list.get(0)
+			);
+			list.set(
+					list.size() - 1,
+					list.get(list.size() - 1) + str.substring(index + variable.name().length())
+			);
+			return list.stream()
+					.flatMap(tmp -> replace(tmp).stream())
+					.toList();
 		}
 		return List.of(str);
 	}
